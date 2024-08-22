58044645fc76d1c53da8dd5996f3105a53696e7d
Inom Dev <inommirzosharipov4@gmail.com>
2024-08-22 18:28:58 +0500

    registration

--- mydb.py
+++ mydb.py
@@ -1,13 +1,13 @@
-import mysql.connector
-
-dataBase = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd='admin'
-)
-
-cursorObject = dataBase.cursor()
-
-cursorObject.execute("CREATE DATABASE crm")
-
-print("All Done!")
+# import mysql.connector
+#
+# dataBase = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     passwd='admin'
+# )
+#
+# cursorObject = dataBase.cursor()
+#
+# cursorObject.execute("CREATE DATABASE crm")
+#
+# print("All Done!")
